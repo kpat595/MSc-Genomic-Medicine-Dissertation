a34a73f15106c2198eb4ded865d2280bbcf425ea
Komal M Patel <260914143+kpat595@users.noreply.github.com>
2026-09-13 23:55:23 +0200

    Delete scripts directory

--- scripts/260513_submit_cellbender.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-bash /well/bsg/projects/CGG0M1_Patel/import_preprocess/scripts/260513_make_cellbender_manifest.sh
-sbatch /well/bsg/projects/CGG0M1_Patel/import_preprocess/scripts/260513_run_cellbender_array.sbatch
